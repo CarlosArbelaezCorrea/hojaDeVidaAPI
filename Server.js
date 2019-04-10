aeec685d72e45ae78cecfc4550e2fec9fd06e790
CarlosArbelaezCorrea <48302457+CarlosArbelaezCorrea@users.noreply.github.com>
2019-04-10 17:28:34 -0500

    isntalacion cors

--- Server.js
+++ Server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 
+const cors = require('cors')
+
 const { hojaDeVida, Vacante, User, dbConnection} = require('./models')
 
 const app = express()
@@ -8,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send({message:"Server on"})
